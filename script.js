4370148a4f8746fb31a2bc369e50022e8feba953
mpociuipa <manthis.poc@gmail.com>
2024-05-08 11:46:57 +0300

    init

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
+// import { getAuth } from "firebase/auth";
 import {getDatabase, set, get, update, remove, ref}
 from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
 
@@ -26,6 +26,10 @@ const db = getDatabase();
 let enterCode = document.getElementById("enterCode");
 let enterName = document.getElementById("enterName");
 let enterQuantity = document.getElementById("enterQuantity");
+let enterPrice = document.getElementById("enterPrice");
+let enterDescription = document.getElementById("enterDescription");
+let enterImages = document.getElementById("enterImages");
+
 let selectCode = document.getElementById("selectCode");
 let selectName = document.getElementById("selectName");
 let selectQuantity = document.getElementById("selectQuantity");
@@ -36,16 +40,42 @@ let updateBtn = document.getElementById("update");
 let removeBtn = document.getElementById("remove");
 let selectBtn = document.getElementById("select");
 
+
 function InsertData(evt) {
   evt.preventDefault();
-  console.log(enterCode.value, enterName.value, enterQuantity.value);
+  //
+  const code = enterCode.value.trim();
+  const name = enterName.value.trim();
+  const quantity = enterQuantity.value.trim();
+  const price = enterPrice.value.trim();
+  const description = enterDescription.value.trim();
+  const images = enterImages.value.trim();
+  if (code.length < 3){
+    alert("Prekes kodas turi buti bent 3 simboliu ilgio.");
+    return;
+  }
+  if (!code || !name || !quantity || !price || !description || !images){
+    alert("Visi laukai turi buti uzpildyti");
+    return;
+  }
+
+  console.log(enterCode.value, enterName.value, enterQuantity.value, enterPrice.value, enterDescription.value, enterImages.value);
   set(ref(db, "Products/" + enterCode.value), {
     Name: enterName.value,
     ID: enterCode.value,
     Quantity: enterQuantity.value,
+    Price: enterPrice.value,
+    Description: enterDescription.value,
+    Images: enterImages.value,
   })
     .then(() => {
       alert("Data added successfully!");
+      enterCode.value = '';
+      enterName.value = '';
+      enterQuantity.value = '';
+      enterPrice.value = '';
+      enterDescription.value = '';
+      enterImages.value = '';
     })
     .catch((error) => {
       alert(error);
@@ -56,10 +86,15 @@ function UpdateData() {
   const code = enterCode.value;
   const newName = enterName.value;
   const newQuantity = enterQuantity.value;
-  console.log(code, newName, newQuantity);
+  const newPrice = enterPrice.value;
+  const newDescription = enterDescription.value;
+  const newImages = enterImages.value;
+  console.log(code, newName, newQuantity, newPrice, newDescription, newImages);
   update(ref(db, "Products/" + code), {
     Name: newName,
-    Quantity: newQuantity
+    Quantity: newQuantity,
+    Price: newPrice,
+    Description: newDescription,
   }).then(() => {
     alert("Data updated successfully!");
   }).catch((error) => {
@@ -81,6 +116,8 @@ function RemoveData() {
 function SelectData() {
   const code = selectCode.value;
   console.log(enterCode.value, enterName.value, enterQuantity.value);
+  
+ 
   get(ref(db, "Products/" + code)).then((snapshot) => {
     if (snapshot.exists()) {
       const data = snapshot.val();
